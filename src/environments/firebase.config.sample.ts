f6b5d0296464ae4845a435a9efe08fbfec6e7cc4
JasonIRL <JasonIRL@users.noreply.github.com>
2020-09-29 10:48:04 -0500

    initial setup

---
+++ src/environments/firebase.config.sample.ts
@@ -0,0 +1,11 @@
+// Rename to firebase.config.ts and paste in your firebase config here
+export const firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
+};
